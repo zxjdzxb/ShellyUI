0d998f9b1a9ce29f5cc6a14418e5a9e6455096b5
jiexingzhou <1660154581@qq.com>
2022-07-13 19:55:44 +0800

    结构优化

---
+++ src/components/SwitchDemo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <h1>Switch 组件示例 </h1>
+    <Demo :component="Switch1Demo" />
+    <Demo :component="Switch2Demo" />
+    <Demo :component="Switch3Demo" />
+  </div>
+</template>
+<script lang="ts">
+
+import Switch1Demo from './Switch1Demo.vue'
+import Switch2Demo from './Switch2Demo.vue'
+import Switch3Demo from './Switch3.demo.vue'
+import Demo from './Demo.vue'
+
+export default {
+  components: {
+    Demo
+  },
+  setup() {
+    return {
+      Switch1Demo,
+      Switch2Demo,
+      Switch3Demo
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$border-color: #d9d9d9;
+
+.mr-4 {
+  border: 1px solid red($color: #000000);
+}
+
+.demo {
+  border: 1px solid $border-color;
+  margin: 16px 0 32px;
+
+  >h2 {
+    font-size: 20px;
+    padding: 8px 16px;
+    border-bottom: 1px solid $border-color;
+  }
+
+  &-component {
+    padding: 16px;
+  }
+
+  &-actions {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+  }
+
+  &-code {
+    padding: 8px 16px;
+    border-top: 1px dashed $border-color;
+
+    >pre {
+      line-height: 1.1;
+      font-family: Consolas, 'Courier New', Courier, monospace;
+      margin: 0;
+    }
+  }
+}
+</style>
